db22d5668512fa880e8249b31fe4d1a5f2c60f1c
Leonardo Gonzalez <leonardo@tangosource.com>
2013-06-02 23:38:36 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pwr::Application.config.secret_key_base = 'd93b465b471ae38f4067ad0ef9d2c882687b506f2faad3092bda33eeee6850c77fdeceb0eb9da78646b5bb17850004ae545f3dd677e5b3d71ffca7882de4d794'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pwr::Application.config.session_store :cookie_store, key: '_pwr_session'
