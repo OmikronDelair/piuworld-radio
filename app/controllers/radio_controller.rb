c6fe0ac75d67b488d99a904e531e67c662507ee9
Leonardo Gonzalez <leonardo@tangosource.com>
2013-06-07 18:01:20 -0500

    Added off air message when no broadcasting detected

--- app/controllers/radio_controller.rb
+++ app/controllers/radio_controller.rb
@@ -8,8 +8,13 @@ class RadioController < ApplicationController
   def track_info
     server = URI.parse("http://cp.freeshoutcast.com/scripts/song2.php?port=40842&host=s3")
     garbage = "\n<style type='text/css'>\nhtml, body, div, span, applet, object, iframe,\nh1, h2, h3, h4, h5, h6, p, blockquote, pre,\na, abbr, acronym, address, big, cite, code,\ndel, dfn, em, font, img, ins, kbd, q, s, samp,\nsmall, strike, strong, sub, sup, tt, var,\ndl, dt, dd, ol, ul, li,\nfieldset, form, label, legend,\ntable, caption, tbody, tfoot, thead, tr, th, td {\n\tmargin: 0;\n\tpadding: 0;\n\tborder: 0;\n\toutline: 0;\n}\n</style>\n<meta content='text/html; charset=utf-8' http-equiv='content-type' />\n<META HTTP-EQUIV='Refresh' CONTENT='30;'>\n\n"
+    server_off ="Stream OFF" 
     track = Net::HTTP.get_response(server).body.gsub(garbage,"")
-    @php = "<b>Estas escuchando:</b> #{track}".html_safe
+    if track.include? server_off 
+      @php = "FUERA DEL AIRE"
+    else
+      @php = "<b>Estas escuchando:</b> #{track}".html_safe
+    end
     render :layout => false
   end
 
