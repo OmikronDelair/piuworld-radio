e46f8b2f58909f64746303a444dd9565747694fe
Leonardo Gonzalez <leonardo@tangosource.com>
2013-06-11 17:30:54 -0500

    Required audio.js dependencies

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,8 @@
 //= require jquery_ujs
 //= require foundation
 //= require turbolinks
+//= require audiojs/audio.min
+//= require audiojs/initialize
 //= require_tree .
 
 $(document).foundation();
