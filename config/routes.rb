6c619d9f78298ba2c9308d6a1101cfd60ccda015
Leonardo Gonzalez <leonardo@tangosource.com>
2013-10-01 06:04:00 -0500

    Changed routes for devise model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Pwr::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :path => 'djs', :path_names => { :sign_in => 'login', :sign_out => 'logout', :password => 'password', :confirmation => 'verification', :unlock => 'unblock', :registration => 'register', :sign_up => 'sign_up' }
+
   root to: "radio#index"
+  get 'djs', to: redirect('/djs/login')
+
   resources :radio do
     collection do
       get :current_track
