dfb591cbdcaeb8a13dbc26559b44912b28e2b022
Leonardo Gonzalez <leonardo@tangosource.com>
2013-10-22 16:09:55 -0500

    Removed traditional JS file for SSEs

--- app/assets/javascripts/radio.js
+++
@@ -1,9 +0,0 @@
-if(this.location.pathname == "/") {
-
-  this.source = new EventSource('/radio/server_status');
-
-  this.source.addEventListener('streaming_status', function(e){
-      $('.track-info').text(e.data.split('"')[1]);
-  });
-
-};
